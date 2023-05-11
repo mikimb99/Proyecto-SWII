19a10daee8c513261707aaee16251faa4817bcb1
Diego Herrero <d.herrero8@usp.ceu.es>
2023-05-11 19:39:19 +0200

    getAll animales

--- routes/animales.js
+++ routes/animales.js
@@ -2,3 +2,16 @@ var express = require('express');
 var router = express.Router();
 const dbo = require('../db/connection');
 const {ObjectID, ObjectId} = require("mongodb");
+
+/* GET all animales. */
+router.get('/', function(req, res, next) {
+    const connection = dbo.getDb();
+    connection.collection('animales').find({}).limit(5)
+      .toArray(function (err, result){
+        if (err){
+          res.status(400).send('Error al buscar animales');
+        } else {
+          res.json(result);
+        }
+      });
+});
