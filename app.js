96df8501a3159ebe8b1ede6e4cdd7b50274f3d4e
Diego Herrero <d.herrero8@usp.ceu.es>
2023-05-11 20:02:19 +0200

    index added

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-var animalesRouter = require('./routes/animales')
+
+
 
 var app = express();
 
@@ -18,7 +19,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+var indexRouter = require('./routes/index')
+var animalesRouter = require('./routes/animales')
 
+app.use('/', indexRouter);
+app.use('/animales', animalesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -35,5 +40,5 @@ app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error');
   });
-  
+
 module.exports = app;
