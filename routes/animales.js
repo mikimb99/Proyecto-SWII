ecdb76da384fe6b95c8d2214a7eaefdad0f4ed07
Diego Herrero <d.herrero8@usp.ceu.es>
2023-05-11 19:44:09 +0200

    // catch 404 and forward to error and // error handler handler and

--- routes/animales.js
+++ routes/animales.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const dbo = require('../db/connection');
+const dbo = require('../db/connect');
 const {ObjectID, ObjectId} = require("mongodb");
 
 /* GET all animales. */
